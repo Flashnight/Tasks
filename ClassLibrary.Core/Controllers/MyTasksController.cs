c38c6af172aeda2a625ffe2cd184d5c5e5fbe3dd
Mikhail Rudgalsky <rudgalsky@gmail.com>
2016-03-21 01:41:06 +0600

    Реализовать отображение списка задач студента
    - Создан класс модели выводимых данных MyTasksListViewModel
    - Создан класс контроллера MyTasksController для отображения данных на
    web-страницу и их фильтрации по выбранной учебной дисциплине
    - Создано временное представление MyTasks/Index

---
+++ ClassLibrary.Core/Controllers/MyTasksController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using ClassLibrary.Core.Models;
+using Microsoft.AspNet.Identity;
+
+namespace ClassLibrary.Core.Controllers
+{
+    public class MyTasksController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index(int? disciplineId)
+        {
+            IQueryable<StudentTask> studentTasks = db.StudentTasks.Include("Discipline");
+
+            string currentUserId = User.Identity.GetUserId();
+
+            studentTasks = studentTasks.Where(p => p.UserId == currentUserId);
+
+            if (disciplineId != null && disciplineId != 0)
+            {
+                studentTasks = studentTasks.Where(p => p.DisciplineId == disciplineId);
+            }
+
+            List<Discipline> disciplines = db.Disciplines.ToList();//список дисциплин
+
+            disciplines.Insert(0, new Discipline { Name = "Все", DisciplineId = 0 });//выбрать все задания
+
+            MyTasksListViewModel mtlvm = new MyTasksListViewModel //данные, которые будут переданы в представление
+            {
+                StudentTasks = studentTasks.ToList(),
+                Disciplines = new SelectList(disciplines, "DisciplineId","name")
+            };
+
+            return View(mtlvm);
+        }
+    }
+}
---
+++ ClassLibrary.Core/Models/MyTasksListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ClassLibrary.Core.Models
+{
+    public class MyTasksListViewModel
+    {
+        public IEnumerable<StudentTask> StudentTasks { get; set; }
+        public SelectList Disciplines { get; set; }
+    }
+}
