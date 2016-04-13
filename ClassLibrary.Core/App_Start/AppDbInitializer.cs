692856f8d08f2d56e4445776cc054be0558d7e75
Mikhail Rudgalsky <rudgalsky@gmail.com>
2016-04-13 21:46:14 +0600

    Реализовать создание ролей для пользователей
    - Реализовано добавление в базу данных следующих ролей для
    пользователей: администратор, преподаватель и студент

---
+++ ClassLibrary.Core/App_Start/AppDbInitializer.cs
@@ -0,0 +1,29 @@
+﻿using ClassLibrary.Core.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace ClassLibrary.Core.App_Start
+{
+    public class AppDbInitializer : CreateDatabaseIfNotExists<ApplicationDbContext>
+    {
+        //Функция добавляет данные по умолчанию в базу данных
+        protected override void Seed(ApplicationDbContext context)
+        {
+            //Создание ролей пользователей
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            //Задание роли администратора, преподавателя и студента
+            var roleAdmin = new IdentityRole { Name = "admin" };
+            var roleTeacher = new IdentityRole { Name = "teacher" };
+            var roleStudent = new IdentityRole { Name = "student" };
+
+            //Сохранение ролей в базе данных
+            roleManager.Create(roleAdmin);
+            roleManager.Create(roleTeacher);
+            roleManager.Create(roleStudent);
+
+            base.Seed(context);
+        }
+    }
+}
--- Tasks/Global.asax.cs
+++ Tasks/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +13,9 @@ namespace Tasks
     {
         protected void Application_Start()
         {
+            //Добавление данных по умолчанию в базу данных
+            Database.SetInitializer<ClassLibrary.Core.Models.ApplicationDbContext>(new ClassLibrary.Core.App_Start.AppDbInitializer());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
