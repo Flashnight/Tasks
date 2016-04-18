f5b9ffa6c5bf464ff61dbdd0df86cb5571956157
Mikhail Rudgalsky <rudgalsky@gmail.com>
2016-04-18 15:31:19 +0600

    Создать класс модели AllStudentsTasksListViewModel
    - Реализован класс модели данных о заданиях, выданных каждому студенту

---
+++ ClassLibrary.Core/Models/AllStudentsTasksListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace ClassLibrary.Core.Models
+{
+    // Модель данных, используемых для выбора заданий всех студентов.
+    //
+    public class AllStudentsTasksListViewModel
+    {
+        // Список заданий всех студентов
+        public IEnumerable<StudentTask> StudentTasks { get; set; }
+        // Список учебных дисциплин.
+        public SelectList Disciplines { get; set; }
+        // Список студентов
+        public SelectList Students { get; set; }
+    }
+}
